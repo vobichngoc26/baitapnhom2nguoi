70194b1f58e3970311c610b1a05de018f959dc70
tlinhnene <linhltt234111e@st.uel.edu.vn>
2024-11-16 16:27:52 +0700

    đã giải quyết xung đột

--- MainWindowExt.py
+++ MainWindowExt.py
@@ -4,7 +4,7 @@ from MainWindow import Ui_MainWindow
 class MainWindowExt(Ui_MainWindow):
     def setupUi(self, MainWindow):
         super().setupUi(MainWindow)
-        self.MainWindow = MainWindow
+        self.MainWindow=MainWindow
     def showWindow(self):
         self.MainWindow.show()
     def setupSignalAndSlot(self):
@@ -14,42 +14,36 @@ class MainWindowExt(Ui_MainWindow):
         self.Tru.clicked.connect(self.processTru)
         self.Nhan.clicked.connect(self.processNhan)
         self.Chia.clicked.connect(self.processChia)
-
     def processExit(self):
         self.MainWindow.close()
-
     def processClear(self):
         self.Hesoa.setText("")
         self.Hesob.setText("")
         self.ketqua.setText("")
         self.Hesoa.setFocus()
-
     def processCong(self):
-        a = float(self.lineEdita.text())
-        b = float(self.lineEditb.text())
-        result = a + b
+        a=float(self.lineEdita.text())
+        b=float(self.lineEditb.text())
+        result=a+b
         self.lineEditkq.setText(f"{a}+{b}={result}")
-
     def processTru(self):
-        a = float(self.lineEdita.text())
-        b = float(self.lineEditb.text())
-        result = a - b
+        a=float(self.lineEdita.text())
+        b=float(self.lineEditb.text())
+        result=a-b
         self.lineEditkq.setText(f"{a}-{b}={result}")
-
     def processNhan(self):
-        a = float(self.lineEdita.text())
-        b = float(self.lineEditb.text())
-        result = a * b
+        a=float(self.lineEdita.text())
+        b=float(self.lineEditb.text())
+        result=a*b
         self.lineEditkq.setText(f"{a}*{b}={result}")
-
     def processChia(self):
         try:
             a = float(self.lineEdita.text())
             b = float(self.lineEditb.text())
-            if b == 0:
-                  self.lineEditkq.setText("Error!")
+            if b==0:
+                self.lineEditkq.setText("Error!")
             else:
-                result = a / b
+                result=a/b
                 self.lineEditkq.setText(f"{a}/{b}={result}")
         except:
             self.lineEditkq.setText("Input data not valid")

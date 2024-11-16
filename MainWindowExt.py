7b6d6ccd3fc44d80f5492981e0a89bcfe8eacfbe
tlinhnene <linhltt234111e@st.uel.edu.vn>
2024-11-16 15:36:08 +0700

    update projects

---
+++ MainWindowExt.py
@@ -0,0 +1,11 @@
+from MainWindow import Ui_MainWindow
+
+
+class MainWindowExt(Ui_MainWindow):
+    def setupUi(self, MainWindow):
+        super().setupUi(MainWindow)
+        self.MainWindow = MainWindow
+    def showWindow(self):
+        self.MainWindow.show()
+    def setupSignalAndSlot(self):
+        pass
